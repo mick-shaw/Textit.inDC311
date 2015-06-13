f3ed27a9693f36c2c078383b3ec240ca6a355c3d
Mick Shaw <mickjshaw@gmail.com>
2015-06-13 13:33:23 -0400

    ..

--- surveyinput.php
+++ surveyinput.php
@@ -19,37 +19,34 @@ $dbconn = pg_connect("host=ec2-54-83-17-8.compute-1.amazonaws.com dbname=d4053sc
          //perform the insert using pg_query
 $result = pg_query($dbconn, "INSERT INTO HBX(callingnumber, questionsanswered, overallsatisfaction, waittime, professionalism) 
                            VALUES('$cn', '$qstn', '$sat','$wait','$prof');");
-if ($qstn = '1'){
+if ($qstn == '1'){
 	$result = pg_query($dbconn, "INSERT INTO questions(_yes) 
                            VALUES('1');");
 }
-if ($qstn = '2'){
+if ($qstn == '2'){
 	$result = pg_query($dbconn, "INSERT INTO questions(_no) 
                            VALUES('1');");
 }
 
-if ($sat = '1'){
+if ($sat == '1'){
 	$result = pg_query($dbconn, "INSERT INTO customerSat(_yes) 
                            VALUES('1');");
-}
-if ($sat = '2'){
+}elseif ($sat == '2'){
 	$result = pg_query($dbconn, "INSERT INTO customerSat(_no) 
                            VALUES('1');");
 }
 
-if ($wait = '1'){
+if ($wait == '1'){
 	$result = pg_query($dbconn, "INSERT INTO wait(_yes) 
                            VALUES('1');");
-}
-if ($wait = '2'){
+}elseif ($wait == '2'){
 	$result = pg_query($dbconn, "INSERT INTO wait(_no) 
                            VALUES('1');");
 }					
-if ($prof = '1'){
+if ($prof == '1'){
 	$result = pg_query($dbconn, "INSERT INTO professionalism(_yes) 
                            VALUES('1');");
-}
-if ($prof = '2'){
+}elseif ($prof == '2'){
 	$result = pg_query($dbconn, "INSERT INTO professionalism(_no) 
                            VALUES('1');");
 }
